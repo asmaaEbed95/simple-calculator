88c9a49fefc4c5dfaabc8055f87cf2b90fe27c68
asmaaEbed <asmaaebed95@gmail.com>
2019-06-26 20:53:41 +0200

    Add files via upload

---
+++ server.php
@@ -0,0 +1,28 @@
+<?php
+
+$no1 = $_REQUEST['n1'];
+$no2 = $_REQUEST['n2'];
+$req = $_REQUEST['req'];
+
+if ($req == 'add'){
+    
+    echo $no1 + $no2;
+    
+}elseif ($req == 'sub') {
+    
+    echo $no1 - $no2;
+    
+}elseif ($req == 'mult') {
+    
+    echo $no1 * $no2;
+    
+}elseif ($req == 'div') {
+    
+    echo $no1 / $no2;
+    
+}
+
+
+
+
+//echo "Hello " . $no1 . " Your age is " . $no2;
